aeb44b1298a387118cdb6f753f983766aaaeaca7
Stefan <stefan.cvetkoski@outlook.com>
2019-05-13 13:00:49 +0200

    Added two helper methods
    - InvokeOnMainThread: Executes code on UI thread synchronously.
    - ToNullableDouble: Tries to convert string to a nullable double type.

---
+++ API/Xamarin.RSControls/Helpers/MainThread.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.RSControls.Helpers
+{
+    public static class MainThread
+    {
+        public static System.Threading.Tasks.Task InvokeOnMainThread(System.Action action)
+        {
+            if (Xamarin.Essentials.MainThread.IsMainThread)
+            {
+                action();
+                return System.Threading.Tasks.Task.CompletedTask;
+            }
+
+            System.Threading.Tasks.TaskCompletionSource<bool> tcs = new System.Threading.Tasks.TaskCompletionSource<bool>();
+            Xamarin.Essentials.MainThread.BeginInvokeOnMainThread(() =>
+            {
+                try
+                {
+                    action();
+                    tcs.SetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+            });
+            tcs.Task.Wait();
+            return tcs.Task;
+        }
+    }
+}
---
+++ API/Xamarin.RSControls/Helpers/StringHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.RSControls.Helpers
+{
+    public static class StringHelpers
+    {
+        public static double? ToNullableDouble(this string s)
+        {
+            double i;
+            if (double.TryParse(s, out i)) return i;
+            return null;
+        }
+    }
+}
