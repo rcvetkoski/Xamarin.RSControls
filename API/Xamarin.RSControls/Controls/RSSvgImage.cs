b349a2ba253ae32d38b940585ce33b3d5aad7c5e
rcvetkoski <radoslav.cvetkoski@gmail.com>
2019-05-13 14:26:17 +0200

    Nuget update + Folder restructuration

--- API/Xamarin.RSControls/Controls/RSSvgImage.cs
+++ API/Xamarin.RSControls/Controls/RSSvgImage.cs
@@ -6,7 +6,7 @@ using SkiaSharp.Views.Forms;
 using System.Linq;
 using System.Reflection;
 
-namespace Xamarin.RSControls
+namespace Xamarin.RSControls.Controls
 {
     public class RSSvgImage : SKCanvasView
     {
